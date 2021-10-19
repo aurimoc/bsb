6c9d4be5323cc1359112f8d47e2c75e358e1148e
aurimoc <aurimas.mockevicius@gmail.com>
2021-10-19 16:27:30 +0200

    .

--- bsb/simulators/nest.py
+++ bsb/simulators/nest.py
@@ -42,7 +42,6 @@ class MapsScaffoldIdentifiers:
 
     def get_nest_ids(self, ids):
         return [self.scaffold_to_nest_map[id] for id in ids]
-        print("Hello world")
 
 
 class NestCell(SimulationCell, MapsScaffoldIdentifiers):
